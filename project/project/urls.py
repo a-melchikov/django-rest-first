f39fcdf2d8f70221570557d8deb5729020f911d7
Andrey Melchikov <andrei.melchikov25@gmail.com>
2024-06-12 23:10:27 +0400

    1 - Serialization passed

--- project/project/urls.py
+++ project/project/urls.py
@@ -25,6 +25,7 @@ router.register(r"users", views.UserViewSet)
 router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),
+    # path("", include(router.urls)),
+    path("", include("snippets.urls")),
     path("api-auth/", include("rest_framework.urls", namespace="restframework")),
 ]
